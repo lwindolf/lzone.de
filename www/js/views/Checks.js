f30e542aa03b4aeb41f695995ca6d58b5c8c7d1b
Lars Windolf <lars.windolf@gmx.de>
2025-05-05 01:11:31 +0200

    Change submodule

--- www/js/views/Checks.js
+++ www/js/views/Checks.js
@@ -8,7 +8,7 @@ export class ChecksView {
 	constructor(el) {
 		el.innerHTML = `
             <h3>SaaS Outages</h3>
-            <x-multistatus-cloud data-path="/js/components/saas-multi-status/" data-reduced="1">ERROR when embedding SaaS MulitStatus</x-multistatus-cloud>
+            <x-multistatus-cloud data-path="/js/components/saas-multi-status/" data-reduced="1">ERROR when embedding SaaS MultiStatus</x-multistatus-cloud>
 
             <h3>DNS Change Detector</h3>
             <x-dns-checker data-path="/js/components/dns-checker/">ERROR when embedding DNS Change Detector!</x-dns-checker>
