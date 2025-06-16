bddfcc896d2dfe2984471a80d27a548fdab8508a
Lars Windolf <lars.windolf@gmx.de>
2025-06-16 23:01:55 +0200

    Relative includes

--- www/js/config.js
+++ www/js/config.js
@@ -3,9 +3,6 @@
 // app customization
 
 export class Config {
-    // trailing slash is important!
-    static baseUrl = 'https://lzone.de/';
-
     // URLs with content indizes to add to the app
     //
     // These are basically Github repos with 
@@ -237,25 +234,25 @@ export class Config {
     // Web components that will embed in the right side toolbox
     //
     // Fields:
-    // - `import`   (mandatory) where to load the component from
+    // - `import`   (mandatory) where to load the component from (import is relative to views/Checks.js)
     // - `embed`    (mandatory) HTML for embedding
     // - `settings` (optional)  HTML for embedding the settings component
     // - `enabled`  (mandatory) if the component is enabled by default
     static toolboxComponents = {
         'SaaS Multi Status': {
-            import     : '/js/components/saas-multi-status/js/MultiStatusCloud.js',
+            import     : '../components/saas-multi-status/js/MultiStatusCloud.js',
             embed      : `<x-multistatus-cloud data-path="/js/components/saas-multi-status/" data-reduced="1">ERROR when embedding SaaS MultiStatus</x-multistatus-cloud>`,
             settings   : `<x-multistatus-settings data-path="/js/components/saas-multi-status/">ERROR when embedding SaaS MultiStatus Settings</x-multistatus-settings>`,
             enabled    : true
         },
         'DNS Change Detector': {
-            import     : '/js/components/dns-checker/js/DnsChecker.js',
+            import     : '../components/dns-checker/js/DnsChecker.js',
             embed      : `<x-dns-checker data-path="/js/components/dns-checker/">ERROR when embedding DNS Change Detector!</x-dns-checker>`,
             settings   : `<x-dns-checker-settings data-path="/js/components/dns-checker/">ERROR when embedding DNS Change Detector!</x-dns-checker-settings>`,
             enabled    : false
         },
         'Pinned Apps': {
-            import     : '/js/components/pinned-apps/js/PinnedApps.js',
+            import     : '../components/pinned-apps/js/PinnedApps.js',
             embed      : `<x-pinned-apps>ERROR when embedding Pinned Apps</x-pinned-apps>`,
             settings   : `<x-pinned-apps-settings>ERROR when embedding Pinned Apps Settings</x-pinned-apps-settings>`,
             enabled    : true
