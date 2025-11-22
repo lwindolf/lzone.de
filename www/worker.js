504e9e80c9360f23e44c4173b78a68502e92e53d
Lars Windolf <lars.windolf@gmx.de>
2025-11-22 23:11:31 +0100

    Bump cache version

--- www/worker.js
+++ www/worker.js
@@ -1,7 +1,7 @@
 // vim: set ts=4 sw=4:
 
 var cachePrefix = 'lzone';
-var cacheVersion = 20251122;
+var cacheVersion = 20251123;
 var cacheName = cachePrefix + '-' + cacheVersion;
 var filesToCache = [
     '/',
