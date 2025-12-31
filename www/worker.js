4c6965e832b4bed2a54a33a01f637b3671631b46
Lars Windolf <lars.windolf@gmx.de>
2025-12-31 01:06:20 +0100

    Update build.

--- www/worker.js
+++ www/worker.js
@@ -1,7 +1,7 @@
 // vim: set ts=4 sw=4:
 
 var cachePrefix = 'lzone';
-var cacheVersion = 20251229;
+var cacheVersion = 20251231;
 var cacheName = cachePrefix + '-' + cacheVersion;
 var filesToCache = [
     '/',
