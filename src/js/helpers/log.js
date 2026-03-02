f0ba66bf48e7f884311cba6a456b9f97473dafa2
Lars Windolf <lars.windolf@gmx.de>
2026-03-02 19:58:33 +0100

    Simply log settings

--- src/js/helpers/log.js
+++ src/js/helpers/log.js
@@ -2,7 +2,8 @@
 
 /* Provides trace topic like filtering of console.log + console.warn
    First word of the msg string is considered the topic name and topics
-   can be enabled by setting window.Config.debug['topic']=true */
+   can be enabled by setting window.debug['topic']=true or
+   window.debug['all']=true to see all log messages */
 
 if(!window.originalConsoleLog) {
     const filteredLog = (msg, ...optionalParams) => {
@@ -13,7 +14,7 @@ if(!window.originalConsoleLog) {
         }
 
         const topic = msg.split(' ')[0];
-        if (window?.Config?.debug[topic] || window?.Config?.debug['all'])
+        if (window?.debug?.[topic] || window?.debug?.['all'])
             window.originalConsoleLog(msg, ...optionalParams);
     };
     window.originalConsoleLog = window.console.log;
@@ -21,4 +22,5 @@ if(!window.originalConsoleLog) {
     window.console.info = filteredLog;
     window.console.warn = filteredLog;
     window.console.debug = filteredLog;
+    window.debug = window.debug || {};
 }
